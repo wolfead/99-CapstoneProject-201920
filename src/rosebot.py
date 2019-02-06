8b83479055bfd6c2daa04945d1f00acfb91b25e6
wolfead <wolfead@rose-hulman.edu>
2019-02-06 10:37:53 -0500

    claimed rosebot module

--- src/rosebot.py
+++ src/rosebot.py
@@ -6,7 +6,7 @@
     -- Lower-level code to interact with the EV3 robot library.
 
   Author:  Your professors (for the framework and lower-level code)
-    and PUT_YOUR_NAMES_HERE.
+    and Alex Wolfe.
   Winter term, 2018-2019.
 """
 
