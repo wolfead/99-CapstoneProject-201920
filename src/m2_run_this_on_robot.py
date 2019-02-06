8964e8b8a7c2cd1e2590cd5472f2cec4b0639a1d
Matt Boutell <boutell@gmail.com>
2019-02-06 09:04:26 -0500

    Add m2 and m3 copies of files

---
+++ src/m2_run_this_on_robot.py
@@ -0,0 +1,24 @@
+"""
+  Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
+  Author:  Your professors (for the framework)
+    and PUT_YOUR_NAME_HERE.
+  Winter term, 2018-2019.
+"""
+
+import rosebot
+import mqtt_remote_method_calls as com
+import time
+
+
+def main():
+    """
+    This code, which must run on the EV3 ROBOT:
+      1. Makes the EV3 robot to various things.
+      2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
+    """
+
+
+# -----------------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# -----------------------------------------------------------------------------
+main()
