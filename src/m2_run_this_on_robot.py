f6190f2dcacac29663eca7e79480d8323af480ac
tabuyoah <tabuyoah@rose-hulman.edu>
2019-02-06 11:07:34 -0500

    I made test code

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -16,7 +16,16 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    run_test_arm()
+    run_test_calibrate()
 
+def run_test_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
+
+def run_test_calibrate():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
