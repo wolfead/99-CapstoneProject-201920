b328d2f2f581086f28cdbe3c05c0c6b96b32972a
smithhm <smithhm@rose-hulman.edu>
2019-02-06 10:39:36 -0500

    Names in shared gui

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Haiden Smith, Alex Wolfe, Alex, Tabuyo
   Winter term, 2018-2019.
 """
 
