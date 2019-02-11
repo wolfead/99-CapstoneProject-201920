5af5d33d42047db6231fda2f335fde60f6fdf12b
tabuyoah <tabuyoah@rose-hulman.edu>
2019-02-11 10:45:24 -0500

    Fixed my m2

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -35,7 +35,7 @@ def real_thing():
 
     while True:
         time.sleep(0.01)
-        if self.time_to_stop == True:
+        if delegate.is_time_to_stop:
             break
 
 def run_test_arm():
