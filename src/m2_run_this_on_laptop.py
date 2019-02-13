79db4c0bd7b16754e38f59612b82389869e09ca3
smithhm <smithhm@rose-hulman.edu>
2019-02-13 11:18:15 -0500

    Grid frames for m1 and m2 were corrected

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -66,7 +66,7 @@ def get_shared_frames(main_frame, mqtt_sender):
 def grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame):
     teleop_frame.grid(row=0, column=0)
     arm_frame.grid(row=1, column=0)
-    control_frame.grid(row=2, column=0)
+    control_frame.grid(row=1, column=1)
     drive_system_frame.grid(row=0, column=1)
 
 
