87b1908cd740b3cf2d9188385e2922227b8d55a2
smithhm <smithhm@rose-hulman.edu>
2019-02-14 13:32:10 -0500

    Understanding GUI graphics

--- sandbox/sandbox3.py
+++ sandbox/sandbox3.py
@@ -1,2 +1,30 @@
 # Put whatever you want in this module and do whatever you want with it.
 # It exists here as a place where you can "try out" things without harm.
+import mqtt_remote_method_calls as com
+import tkinter
+from tkinter import *
+import time
+import random
+
+
+#assign the module to a var
+tk = Tk()
+
+#set global vars
+Height = 500
+Width = 500
+
+canvas = Canvas(tk, width=Width, height=Height)
+tk.title("Graphics")
+canvas.pack()
+
+ball = canvas.create_oval(0, 0, 100, 100, fill='red')
+hat = canvas.create_text(10, 50, text="hotdog")
+xspeed = 5
+yspeed = 5
+
+while True:
+    canvas.move(hat, xspeed, yspeed)
+
+    tk.update()
+    time.sleep(0.1)
